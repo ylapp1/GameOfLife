dff293aaa24e1530111d6a2f597071f278edb9a8
tschreindl <tim.schreindl@cn-consult.eu>
2017-08-15 16:55:46 +0200

    Fixed some bugs
    Cleaned up and fixed bugs

--- src/Classes/Inputs/UserInput.php
+++ src/Classes/Inputs/UserInput.php
@@ -26,9 +26,9 @@ class UserInput extends BaseInput
     function fillBoard($_board, $_options)
     {
         echo "Set the coordinates for the living fields as below:\n";
-        echo "<number>,<number>\n";
-        echo "and after that press <Enter>. The first number stands for X and the second number for Y\n";
-        echo "The game starts when you type \"start\" in a new line and press <Enter>\n";
+        echo "<"."number>,<number".">\n";
+        echo "and after that press <"."Enter>. The first number stands for X and the second number for Y\n";
+        echo "The game starts when you type \"start\" in a new line and press <"."Enter>\n";
         echo "Let's Go:\n";
 
         $fileOpen = fopen('php://stdin','r') or die($php_errormsg);
@@ -36,25 +36,30 @@ class UserInput extends BaseInput
 
         while (! $lastLine) {
             $nextLine = fgets($fileOpen,1024);
-            if ("start\n" == strtolower($nextLine))
+            if (stristr($nextLine, "start"))
             {
                 $lastLine = true;
             }
             else
             {
+                if (stristr($nextLine, "exit")) die();
                 if (stristr($nextLine, ","))
                 {
 
                     $inputSplits = explode(",", $nextLine);
                     if (count($inputSplits) == 2)
                     {
-                        if ($inputSplits[0] > $_board->width() - 1 || $inputSplits[0] > $_board->height() - 1)
+                        if ($inputSplits[0] > $_board->width() - 1 || $inputSplits[1] > $_board->height() - 1)
                         {
                             echo "The numbers may not be larger than the field!";
                         }else
                         {
                             $_board->setField((int)$inputSplits[0], (int)$inputSplits[1], true);
                         }
+                    }
+                    else
+                    {
+                        echo "Don't give me more than two numbers!";
                     }
                 }
                 $_board->printBoard();
