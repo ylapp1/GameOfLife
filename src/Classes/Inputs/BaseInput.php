c4be7d3899d8826314159f2064b1200d39683f6a
Yannick Lapp <yannick.lapp@cn-consult.eu>
2017-08-15 10:24:48 +0200

    Add method comments to BaseInput.

--- src/Classes/Inputs/BaseInput.php
+++ src/Classes/Inputs/BaseInput.php
@@ -9,16 +9,32 @@
 namespace Input;
 
 
+use Ulrichsg\Getopt;
+use GameOfLife\Board;
+
+
 /**
  * Class BaseInput
  */
 class BaseInput
 {
+    /**
+     * Adds object specific options
+     *
+     * @param Getopt $_options     Options to which the object specific options shall be added
+     */
     public function addOptions($_options)
     {
 
     }
 
+
+    /**
+     * Fills a given board with cells
+     *
+     * @param Board $_board      The board which shall be filled with cells
+     * @param Getopt $_options    Object specific options (e.g. posX, posY, fillPercent)
+     */
     public function fillBoard($_board, $_options)
     {
 
