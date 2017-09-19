9409546b221ed49f95d783c97329599c227f0af5
Yannick Lapp <yannick.lapp@cn-consult.eu>
2017-09-19 10:08:54 +0200

    Changed autoloader

--- tests/testboot.php
+++ tests/testboot.php
@@ -6,9 +6,9 @@
  * @author Yannick Lapp <yannick.lapp@cn-consult.eu>
  */
 
-require_once __DIR__ . '/../vendor/composer/autoload_real.php';
+$loader = require_once __DIR__ . '/../vendor/composer/autoload_real.php';
 
-$loader =  ComposerAutoloaderInitef2f69864c5695a57d28bbd4a98f5b03::getLoader();
+//$loader =  ComposerAutoloaderInitef2f69864c5695a57d28bbd4a98f5b03::getLoader();
 
 $loader->addPsr4("GameOfLife\\", __DIR__ . "/../src/GameOfLife");
 $loader->addPsr4("Input\\", __DIR__ . "/../src/GameOfLife/Inputs");
