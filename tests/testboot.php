f42d7d898263268934490eae024e124935862fd5
Yannick Lapp <yannick.lapp@cn-consult.eu>
2017-09-19 10:32:13 +0200

    Fixed autoloader and ImageCreatorTest

--- tests/testboot.php
+++ tests/testboot.php
@@ -6,9 +6,7 @@
  * @author Yannick Lapp <yannick.lapp@cn-consult.eu>
  */
 
-//require_once __DIR__ . '/../vendor/composer/autoload_real.php';
-//$loader =  ComposerAutoloaderInitef2f69864c5695a57d28bbd4a98f5b03::getLoader();
-$loader = require_once __DIR__ . "/../vendor/autoload.php";
+$loader = require __DIR__ . "/../vendor/autoload.php";
 
 $loader->addPsr4("GameOfLife\\", __DIR__ . "/../src/GameOfLife");
 $loader->addPsr4("Input\\", __DIR__ . "/../src/GameOfLife/Inputs");
