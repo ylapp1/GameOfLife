196fb410b6619bb4aaef65ff44e578371009dbaf
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-01-12 11:23:13 +0100

    Add vagrant box
    * Added a vagrant box for this project
    * Updated the composer.lock file to be compatible with the vagrant box
    * Added .vagrant to .gitignore

---
+++ VagrantProvision.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Package list
+packages=(
+
+  # PHP7
+  php7.0-common php7.0-cli
+
+  ## The zip package is needed for composer
+  php7.0-zip
+
+  ## phpunit dependencies
+  php7.0-xml php7.0-mbstring php-xdebug
+
+  ## ImageOutput dependencies
+  php7.0-gd
+)
+
+apt-get update
+apt-get install -y "${packages[@]}"
+
+
+# Install composer
+cd /usr/local/bin
+EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet --filename=composer
+RESULT=$?
+rm composer-setup.php
+chmod a+x composer
+cd
+
+
+# Fixing the timezone
+
+# Change the system timezone (fixes time for web interface)
+timedatectl set-timezone Europe/Berlin
+
+# Change the php timezone (fixes time for cli commands)
+sed -i "/;date.timezone =/c date.timezone = \"Europe/Berlin\"" /etc/php/7.0/cli/php.ini
+
+
+# Fixes the bug that backspace prints a character in a CMD vagrant ssh session
+echo "stty sane" >> /home/ubuntu/.bashrc
---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant.configure("2") do |config|
+ 
+  config.vm.box = "ubuntu/xenial64"
+
+  config.vm.provision "shell", path: "VagrantProvision.sh"
+  
+  # Disable creation of ubuntu-xenial-16.04-cloudimg-console.log
+  config.vm.provider "virtualbox" do |vb|
+    vb.customize [ "modifyvm", :id, "--uartmode1", "disconnected" ]
+  end
+  
+end
