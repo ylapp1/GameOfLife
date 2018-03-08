82c4b3b61c857776144e6ef7fb80fb1eea418ee9
ylapp1 <30892746+ylapp1@users.noreply.github.com>
2018-03-08 10:36:39 +0100

    Squash feature/move-content-of-gameoflife.php-to-classes (#19)
    * Moved the glue code from gameoflife.php to own classes.
    * Added unit tests for the new classes

---
+++ src/GameOfLife/GameOfLife.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @file
+ * @version 0.1
+ * @copyright 2018 CN-Consult GmbH
+ * @author Yannick Lapp <yannick.lapp@cn-consult.eu>
+ */
+
+namespace GameOfLife;
+
+use OptionHandler\OptionHandler;
+use Input\BaseInput;
+use Output\BaseOutput;
+use Ulrichsg\Getopt;
+
+/**
+ * Wrapper class for the GameOfLife simulation.
+ */
+class GameOfLife
+{
+    /**
+     * The option list
+     *
+     * @var Getopt $options
+     */
+    private $options;
+
+    /**
+     * The option handler
+     *
+     * @var OptionHandler The option handler
+     */
+    private $optionHandler;
+
+    /**
+     * The board that is used for the simulation.
+     *
+     * @var Board $board
+     */
+    private $board;
+
+    /**
+     * The input
+     *
+     * @var BaseInput $input
+     */
+    private $input;
+
+    /**
+     * The output
+     *
+     * @var BaseOutput $output
+     */
+    private $output;
+
+    /**
+     * The game logic
+     *
+     * @var GameLogic $gameLogic
+     */
+    private $gameLogic;
+
+
+    /**
+     * GameOfLife constructor.
+     */
+    public function __construct()
+    {
+        $this->options = new Getopt();
+        $this->optionHandler = new OptionHandler();
+    }
+
+
+    /**
+     * Parses the options and returns whether the simulation shall be started or not.
+     *
+     * @return Bool Indicates whether the simulation shall be started or not
+     */
+    public function initialize(): Bool
+    {
+        $this->optionHandler->initializeOptions($this->options);
+        $this->options->parse();
+
+        $generalOptionUsed = $this->optionHandler->optionParser()->parseGeneralOptions($this->options);
+        if ($generalOptionUsed) return false;
+
+        $board = $this->optionHandler->optionParser()->parseBoardOptions($this->options);
+        if (! $board) return false;
+        else $this->board = $board;
+
+        $this->input = $this->optionHandler->optionParser()->parseInputOptions($this->options);
+        $this->output = $this->optionHandler->optionParser()->parseOutputOptions($this->options);
+
+        $rule = $this->optionHandler->optionParser()->parseRuleOptions($this->options);
+        $this->gameLogic = new GameLogic($rule);
+
+        return true;
+    }
+
+    /**
+     * Starts the game loop.
+     */
+    public function startSimulation()
+    {
+        $this->input->fillBoard($this->board, $this->options);
+        $this->output->startOutput($this->options, $this->board);
+
+        // Game loop
+        while (! ($this->gameLogic->isMaxStepsReached($this->board) || $this->gameLogic->isLoopDetected() || $this->gameLogic->isBoardEmpty($this->board)))
+        {
+            $this->output->outputBoard($this->board);
+            $this->gameLogic->calculateNextBoard($this->board);
+        }
+
+        $this->output->finishOutput();
+    }
+}
---
+++ tests/GameOfLife/OptionHandler/OptionParserTest.php
@@ -0,0 +1,314 @@
+<?php
+/**
+ * @file
+ * @version 0.1
+ * @copyright 2018 CN-Consult GmbH
+ * @author Yannick Lapp <yannick.lapp@cn-consult.eu>
+ */
+
+use GameOfLife\Board;
+use OptionHandler\OptionHandler;
+use OptionHandler\OptionParser;
+use PHPUnit\Framework\TestCase;
+use Ulrichsg\Getopt;
+
+/**
+ * Checks whether the option parser works as expected.
+ */
+class OptionParserTest extends TestCase
+{
+    /**
+     * The options list mock
+     *
+     * @var \PHPUnit\Framework\MockObject\MockObject $optionsMock
+     */
+    private $optionsMock;
+
+    /**
+     * The option parser
+     *
+     * @var OptionParser $optionParser
+     */
+    private $optionParser;
+
+    /**
+     * The parent option handler mock
+     *
+     * @var \PHPUnit\Framework\MockObject\MockObject $parentOptionHandlerMock
+     */
+    private $parentOptionHandlerMock;
+
+
+    /**
+     * Function that is called before each test.
+     */
+    protected function setUp()
+    {
+        $this->optionsMock = $this->getMockBuilder(\Ulrichsg\Getopt::class)
+                                  ->getMock();
+
+        $this->parentOptionHandlerMock = $this->getMockBuilder(\OptionHandler\Optionhandler::class)
+                                              ->getMock();
+
+        if ($this->parentOptionHandlerMock instanceof OptionHandler)
+        {
+            $this->optionParser = new OptionParser($this->parentOptionHandlerMock);
+        }
+    }
+
+    /**
+     * Function that is called after each test.
+     */
+    protected function tearDown()
+    {
+        unset($this->optionsMock);
+        unset($this->optionhandlerMock);
+        unset($this->optionParser);
+        unset($this->parentOptionHandlerMock);
+    }
+
+
+    /**
+     * Checks whether the optionParser can parse general options.
+     *
+     * @covers \OptionHandler\OptionParser::__construct()
+     * @covers \OptionHandler\OptionParser::parseGeneralOptions()
+     */
+    public function testCanParseGeneralOptions()
+    {
+        $this->optionsMock->expects($this->exactly(5))
+                          ->method("getOption")
+                          ->withConsecutive(array("version"), array("version"), array("help"), array("version"), array("help"))
+                          ->willReturn(true, null, true, null, null);
+
+        if ($this->optionsMock instanceof Getopt)
+        {
+            // Hide output
+            $this->expectOutputRegex("/.*/");
+            $generalOptionCalled = $this->optionParser->parseGeneralOptions($this->optionsMock);
+            $this->assertTrue($generalOptionCalled);
+
+            $generalOptionCalled = $this->optionParser->parseGeneralOptions($this->optionsMock);
+            $this->assertTrue($generalOptionCalled);
+
+            $generalOptionCalled = $this->optionParser->parseGeneralOptions($this->optionsMock);
+            $this->assertFalse($generalOptionCalled);
+        }
+    }
+
+    /**
+     * Checks whether the option parser can parse board options.
+     *
+     * @param array $_returnValueMaps The return values for "getOption" in the format array("optionName", "returnValue")
+     * @param Board|Bool $_expectedBoard The expected Board object or false
+     *
+     * @dataProvider parseBoardOptionsProvider
+     * @covers \OptionHandler\OptionParser::parseBoardOptions()
+     */
+    public function testCanParseBoardOptions(array $_returnValueMaps, $_expectedBoard)
+    {
+        $this->optionsMock->expects($this->exactly(count($_returnValueMaps)))
+            ->method("getOption")
+            ->willReturnMap($_returnValueMaps);
+
+
+        if ($this->optionsMock instanceof Getopt)
+        {
+            // Hide output
+            $this->expectOutputRegex("/.*/");
+
+            $board = $this->optionParser->parseBoardOptions($this->optionsMock);
+            $this->assertEquals($_expectedBoard, $board);
+        }
+    }
+
+    /**
+     * DataProvider for OptionParserTest::testCanParseBoardOptions().
+     *
+     * @return array Test values in the format array(getopt values, expected Board object)
+     */
+    public function parseBoardOptionsProvider(): array
+    {
+        return array(
+            "Custom values with border" => array(
+                array(
+                    array("width", "1"),
+                    array("width", "1"),
+                    array("height", "2"),
+                    array("height", "2"),
+                    array("maxSteps", "15"),
+                    array("maxSteps", "15"),
+                    array("border", "solid"),
+                    array("border", "solid")
+                ),
+                new Board(1, 2, 15, true)
+            ),
+            "Custom values without border" => array(
+                array(
+                    array("width", "5"),
+                    array("width", "5"),
+                    array("height", "18"),
+                    array("height", "18"),
+                    array("maxSteps", "200"),
+                    array("maxSteps", "200"),
+                    array("border", "passthrough"),
+                    array("border", "passthrough")
+                ),
+                new Board(5, 18, 200, false)
+            ),
+            "Custom values with invalid border type" => array(
+                array(
+                    array("width", "19"),
+                    array("width", "19"),
+                    array("height", "13"),
+                    array("height", "13"),
+                    array("maxSteps", "7"),
+                    array("maxSteps", "7"),
+                    array("border", "glass"),
+                    array("border", "glass")
+                ),
+                false
+            ),
+            "Default values" => array(
+                array(
+                    array("width", null),
+                    array("height", null),
+                    array("maxSteps", null),
+                    array("border", null)
+                ),
+                new Board(20, 10, 50, true)
+            )
+        );
+    }
+
+
+    /**
+     * Checks whether input options are parsed correctly.
+     *
+     * @param String $_optionParseFunction The function that will be tested
+     * @param array $_returnValueMaps The return values for "getOption" in the format array("optionName", "returnValue")
+     * @param String $_expectedClass The expected input class that is returned from parsing the input options
+     * @param String[] $_excludeClasses The excluded classes
+     * @param array $_linkedOptions The linked options array
+     *
+     * @dataProvider parseClassOptionsProvider()
+     * @covers \OptionHandler\OptionParser::parseInputOptions()
+     * @covers \OptionHandler\OptionParser::parseOutputOptions()
+     * @covers \OptionHandler\OptionParser::parseRuleOptions()
+     */
+    public function testCanParseClassOptions(String $_optionParseFunction, array $_returnValueMaps, String $_expectedClass, array $_excludeClasses, array $_linkedOptions = array())
+    {
+        $this->optionsMock->expects($this->exactly(count($_returnValueMaps)))
+                          ->method("getOption")
+                          ->willReturnmap($_returnValueMaps);
+
+        if (count($_excludeClasses) > 0)
+        {
+            $this->parentOptionHandlerMock->expects($this->exactly(1))
+                                          ->method("excludeClasses")
+                                          ->willReturn($_excludeClasses);
+        }
+
+        $this->parentOptionHandlerMock->expects($this->exactly(1))
+                                      ->method("linkedOptions")
+                                      ->willReturn($_linkedOptions);
+
+        if ($this->optionsMock instanceof Getopt)
+        {
+            $input = $this->optionParser->$_optionParseFunction($this->optionsMock);
+            $this->assertInstanceOf($_expectedClass, $input);
+        }
+    }
+
+    /**
+     * DataProvider for OptionParserTest::testCanParseClassOptions().
+     *
+     * @return array Test values in the format array(getOption values, expectedClass, classExists, linkedOptions)
+     */
+    public function parseClassOptionsProvider(): array
+    {
+        return array(
+
+            // Input
+            "input = Valid class name (User)" => array(
+                "parseInputOptions",
+                array(array("input", "User"), array("input", "User")),
+                \Input\UserInput::class,
+                array("BaseInput")
+            ),
+            "input = Excluded class name (Base)" => array(
+                "parseInputOptions",
+                array(array("input", "Base"), array("input", "Base")),
+                \Input\RandomInput::class,
+                array("BaseInput")
+            ),
+            "input = Invalid class name (HelloWorld)" => array(
+                "parseInputOptions",
+                array(array("input", "HelloWorld"), array("input", "HelloWorld")),
+                \Input\RandomInput::class,
+                array(),
+            ),
+            "input = test, template = true" => array(
+                "parseInputOptions",
+                array(array("input", "test"), array("input", "test"), array("template", true)),
+                \Input\TemplateInput::class,
+                array(),
+                array("template" => "\Input\TemplateInput")
+            ),
+
+            // Output
+            "output = Valid class name (Gif)" => array(
+                "parseOutputOptions",
+                array(array("output", "Gif"), array("output", "Gif")),
+                \Output\GifOutput::class,
+                array("BaseOutput")
+            ),
+            "output = Excluded class name (Base)" => array(
+                "parseOutputOptions",
+                array(array("output", "Base"), array("output", "Base")),
+                \Output\ConsoleOutput::class,
+                array("BaseOutput")
+            ),
+            "output = Invalid class name (HelloWorld)" => array(
+                "parseOutputOptions",
+                array(array("output", "HelloWorld"), array("output", "HelloWorld")),
+                \Output\ConsoleOutput::class,
+                array()
+            ),
+            "output = test, videoOutputAddSound = true" => array(
+                "parseOutputOptions",
+                array(array("output", "test"), array("output", "test"), array("videoOutputAddSound", true)),
+                \Output\VideoOutput::class,
+                array(),
+                array("videoOutputAddSound" => "\Output\VideoOutput")
+            ),
+
+            // Rule
+            "rules = Valid class name (Copy)" => array(
+                "parseRuleOptions",
+                array(array("rules", "Copy"), array("rules", "Copy")),
+                \Rule\CopyRule::class,
+                array("BaseRule")
+            ),
+            "rules = Excluded class name (Base)" => array(
+                "parseRuleOptions",
+                array(array("rules", "Base"), array("rules", "Base")),
+                \Rule\ConwayRule::class,
+                array("BaseRule")
+            ),
+            "rules = Invalid class name (HelloWorld)" => array(
+                "parseRuleOptions",
+                array(array("rules", "HelloWorld"), array("rules", "HelloWorld")),
+                \Rule\ConwayRule::class,
+                array()
+            ),
+            "rules = test, testRuleOption = true" => array(
+                "parseRuleOptions",
+                array(array("rules", "test"), array("rules", "test"), array("testRuleOption", true)),
+                \Rule\Two45Rule::class,
+                array(),
+                array("testRuleOption" => "\Rule\Two45Rule")
+            )
+        );
+    }
+}
