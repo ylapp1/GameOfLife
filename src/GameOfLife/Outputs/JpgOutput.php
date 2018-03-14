1e27472d326f36dc74f43bca05b3a0d57d4bef24
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-03-14 13:03:09 +0100

    Add JpgOutput class

---
+++ src/GameOfLife/Outputs/JpgOutput.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @file
+ * @version 0.1
+ * @copyright 2018 CN-Consult GmbH
+ * @author Yannick Lapp <yannick.lapp@cn-consult.eu>
+ */
+
+namespace Output;
+
+use GameOfLife\Board;
+use Ulrichsg\Getopt;
+use Utils\FileSystemHandler;
+
+/**
+ * Saves the game steps in .jpg files.
+ */
+class JpgOutput extends ImageOutput
+{
+    /**
+     * JpgOutput constructor.
+     */
+    public function __construct()
+    {
+        $this->fileSystemHandler = new FileSystemHandler();
+        $outputDirectory = $this->baseOutputDirectory . "/JPG/Game_" . $this->getNewGameId("JPG");
+        parent::__construct("jpg", $outputDirectory);
+    }
+
+
+    /**
+     * Displays a text to the user wihch tells the user that the simulation starts.
+     *
+     * @param Getopt $_options User inputted option list
+     * @param Board $_board Initial board
+     */
+    public function startOutput(Getopt $_options, Board $_board)
+    {
+        parent::startOutput($_options, $_board);
+        echo "Starting JPG Output ...\n\n";
+    }
+
+    /**
+     * Outputs one game step.
+     *
+     * @param Board $_board Current board
+     */
+    public function outputBoard(Board $_board)
+    {
+        echo "\rGamestep: " . ($_board->gameStep() + 1);
+        $image = $this->imageCreator->createImage($_board);
+
+        $this->fileSystemHandler->createDirectory($this->imageOutputDirectory());
+
+        $fileName = $_board->gameStep() . ".jpg";
+        $filePath = $this->imageOutputDirectory() . "/" . $fileName;
+
+        imagejpeg($image, $filePath);
+        unset($image);
+    }
+
+    /**
+     * Displays a text which tells the user that the simulation is finished.
+     *
+     * @param String $_simulationEndReason The reason why the simulation ended
+     */
+    public function finishOutput(String $_simulationEndReason)
+    {
+        parent::finishOutput($_simulationEndReason);
+        unset($this->imageCreator);
+    }
+}
--- tests/GameOfLife/OptionHandler/OptionHandlerTest.php
+++ tests/GameOfLife/OptionHandler/OptionHandlerTest.php
@@ -38,7 +38,7 @@ class OptionHandlerTest extends TestCase
 
         $optionHandler->initializeOptions($options);
 
-        $this->assertEquals(45, count($options->getOptionList()));
-        $this->assertEquals(35, count($optionHandler->linkedOptions()));
+        $this->assertEquals(49, count($options->getOptionList()));
+        $this->assertEquals(39, count($optionHandler->linkedOptions()));
     }
 }
