eaee454a47ed1d9d93238dcca46e383af14285e6
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-04-06 11:48:00 +0200

    Add parameters to move selection option

--- src/GameOfLife/Inputs/BoardEditor/Options/MoveSelectionOption.php
+++ src/GameOfLife/Inputs/BoardEditor/Options/MoveSelectionOption.php
@@ -29,20 +29,23 @@ class MoveSelectionOption extends BoardEditorOption
         $this->aliases = array("moveSelection");
         $this->callback = "moveSelection";
         $this->description = "Moves the selection to another area on the board";
-        $this->arguments = array();
+        $this->arguments = array("X-Coordinate (left border)" => "int", "Y-Coordinate (top border)" => "int");
     }
 
     /**
      * Moves the selection to another area on the board.
      *
+     * @param int $_posX The X-Position of the top left corner of the template on the board
+     * @param int $_posY The Y-Position of the top left corner of the template on the board
+     *
      * @return bool Indicates whether the board editing is finished
      *
      * @throws \Exception The "cut" and "paste" exceptions
      */
-    public function moveSelection(): Bool
+    public function moveSelection(int $_posX, int $_posY): Bool
     {
         $this->parentBoardEditor->optionHandler()->parseInput("cut");
-        $this->parentBoardEditor->optionHandler()->parseInput("paste");
+        $this->parentBoardEditor->optionHandler()->parseInput("paste " . $_posX . " " . $_posY);
 
         $this->parentBoardEditor->outputBoard();
         return false;
