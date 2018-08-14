fecd134f8522f24d7019d0d527c6112f121aeb19
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-08-14 16:05:34 +0200

    Improve documentation of ImageCanvas

--- src/GameOfLife/Outputs/BoardRenderer/Image/ImageCanvas.php
+++ src/GameOfLife/Outputs/BoardRenderer/Image/ImageCanvas.php
@@ -15,26 +15,75 @@ use BoardRenderer\Base\BaseCanvas;
  */
 class ImageCanvas extends BaseCanvas
 {
+	// Constructor
+
+	/**
+	 * The background image of the canvas.
+	 *
+	 * @var resource $backgroundImage
+	 */
 	private $backgroundImage;
+
+	/**
+	 * The total image (border and cell images combined)
+	 *
+	 * @var resource $image
+	 */
 	private $image;
 
+	/**
+	 * The field size in pixels
+	 *
+	 * @var int $fieldSize
+	 */
 	private $fieldSize;
 
 
+	// Magic Methods
+
+	/**
+	 * ImageCanvas constructor.
+	 *
+	 * @param int $_fieldSize The field size in pixels
+	 */
 	public function __construct(int $_fieldSize)
 	{
 		$this->fieldSize = $_fieldSize;
 	}
 
+
+	// Class Methods
+
+	/**
+	 * Resets the total image.
+	 */
+	public function reset()
+	{
+		$this->image = null;
+	}
+
+	/**
+	 * Adds the rendered border grid to the canvas.
+	 *
+	 * @param resource $_renderedBorderGrid The rendered border grid
+	 */
 	public function addRenderedBorderGrid($_renderedBorderGrid)
 	{
 		$this->backgroundImage = $_renderedBorderGrid;
 	}
 
+	/**
+	 * Adds the rendered board fields to the total image.
+	 *
+	 * @param resource[][] $_renderedBoardFields The list of rendered board fields
+	 */
 	public function addRenderedBoardFields(array $_renderedBoardFields)
 	{
-		$this->image = imagecreate(imagesx($this->backgroundImage), imagesy($this->backgroundImage));
-		imagecopy($this->image, $this->backgroundImage, 0,0,0,0,imagesx($this->backgroundImage), imagesy($this->backgroundImage));
+		$backgroundImageWidth = imagesx($this->backgroundImage);
+		$backgroundImageHeight = imagesy($this->backgroundImage);
+
+		$this->image = imagecreate($backgroundImageWidth, $backgroundImageHeight);
+		imagecopy($this->image, $this->backgroundImage, 0,0,0,0, $backgroundImageWidth, $backgroundImageHeight);
 
 		foreach ($_renderedBoardFields as $y => $renderedBoardFieldRow)
 		{
@@ -55,13 +104,13 @@ class ImageCanvas extends BaseCanvas
 		}
 	}
 
+	/**
+	 * Returns the content of the canvas.
+	 *
+	 * @return resource The content of the canvas
+	 */
 	public function getContent()
 	{
 		return $this->image;
-	}
-
-	public function reset()
-	{
-		$this->image = null;
 	}
 }
