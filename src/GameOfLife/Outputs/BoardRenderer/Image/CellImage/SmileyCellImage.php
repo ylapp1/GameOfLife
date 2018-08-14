42e20b6aaf2533619cc012a2be236c6b00d91bb4
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-08-14 15:32:53 +0200

    Fix and improve smiley cell image

--- src/GameOfLife/Outputs/BoardRenderer/Image/CellImage/SmileyCellImage.php
+++ src/GameOfLife/Outputs/BoardRenderer/Image/CellImage/SmileyCellImage.php
@@ -12,6 +12,7 @@ use Output\Helpers\ImageColor;
 
 /**
  * Creates and returns a cell smiley image.
+ * The smiley image looks like a regular smiling smiley (like this: ☻)
  */
 class SmileyCellImage extends TransparentCellImage
 {
@@ -46,13 +47,15 @@ class SmileyCellImage extends TransparentCellImage
 		imagefill($cellImage, 0, 0, $transparentColor->getColor($cellImage));
 
 		// Head
-		imagefilledellipse($cellImage, $this->width / 2, $this->height / 2, $this->width, $this->height, $this->foreGroundColor->getColor($cellImage));
+		imagefilledellipse($cellImage, $this->width * 0.5, $this->height * 0.5, $this->width - 1, $this->height - 1, $this->foreGroundColor->getColor($cellImage));
+
 		// Eyes
-		imagefilledellipse($cellImage, $this->width / 4, $this->height / 4, $this->width / 4, $this->height / 4, $this->backgroundColor->getColor($cellImage));
-		imagefilledellipse($cellImage, $this->width * 3/4, $this->height / 4, $this->width / 4, $this->height / 4, $this->backgroundColor->getColor($cellImage));
+		imagefilledellipse($cellImage, $this->width * 0.25, $this->height * 3/8, $this->width * 0.2, $this->height * 0.2, $this->backgroundColor->getColor($cellImage));
+		imagefilledellipse($cellImage, $this->width * 0.75, $this->height * 3/8, $this->width * 0.2, $this->height * 0.2, $this->backgroundColor->getColor($cellImage));
+
 		// Mouth
 		imagesetthickness($cellImage, 5);
-		imagearc($cellImage, $this->width / 2, $this->height / 2, $this->width * 3/4,$this->height * 3/4, 25, 155, $this->backgroundColor->getColor($cellImage));
+		imagearc($cellImage, $this->width * 0.5, $this->height * 5/8, $this->width * 0.6,$this->height * 0.5, 10, 170, $this->backgroundColor->getColor($cellImage));
 
 		imagecolortransparent($cellImage, $transparentColor->getColor($cellImage));
 
