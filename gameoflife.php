82c8431013df7b30f3c0ef3fc231704ef58515e2
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-09-21 08:45:37 +0200

    Move BoardRenderer folder to GameOfLife folder

--- gameoflife.php
+++ gameoflife.php
@@ -8,7 +8,7 @@
 
 $loader = require_once(__DIR__ . "/vendor/autoload.php");
 $loader->addPsr4("BoardEditor\\", __DIR__ . "/src/GameOfLife/BoardEditor");
-$loader->addPsr4("BoardRenderer\\", __DIR__ . "/src/GameOfLife/Outputs/BoardRenderer");
+$loader->addPsr4("BoardRenderer\\", __DIR__ . "/src/GameOfLife/BoardRenderer");
 $loader->addPsr4("GameOfLife\\", __DIR__ . "/src/GameOfLife");
 $loader->addPsr4("Input\\", __DIR__ . "/src/GameOfLife/Inputs");
 $loader->addPsr4("OptionHandler\\", __DIR__ . "/src/GameOfLife/OptionHandler");
