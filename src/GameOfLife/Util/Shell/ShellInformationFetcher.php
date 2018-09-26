b4bca87c923f0d9d0be22fd9c174736deb1c012c
Yannick Lapp <yannick.lapp@cn-consult.eu>
2018-09-26 16:20:39 +0200

    Add windows commands to fetch height/width of window

--- src/GameOfLife/Util/Shell/ShellInformationFetcher.php
+++ src/GameOfLife/Util/Shell/ShellInformationFetcher.php
@@ -31,6 +31,20 @@ class ShellInformationFetcher
      */
     private $shellExecutor;
 
+	/**
+	 * The cached number of shell lines
+	 *
+	 * @var int $cachedNumberOfShellLines
+	 */
+    static $cachedNumberOfShellLines;
+
+	/**
+	 * The cached number of shell columns
+	 *
+	 * @var int $cachedNumberOfShellColumns
+	 */
+    static $cachedNumberOfShellColumns;
+
 
     // Magic Methods
 
@@ -48,78 +62,82 @@ class ShellInformationFetcher
 
     /**
      * Returns the number of shell lines.
+     * The number of shell lines is only fetched once, after that the cached number of shell lines will be returned.
      *
      * @return int The number of shell lines
      */
     public function getNumberOfShellLines(): int
     {
-        /*
-         * 29 lines is the default number of lines in cmd
-         * 50 lines is the default number of lines in PowerShell
-         */
-        $numberOfLines = 29;
+    	if (! $this::$cachedNumberOfShellLines)
+	    {
+		    /*
+		     * 29 lines is the default number of lines in cmd
+		     * 50 lines is the default number of lines in PowerShell
+		     */
+		    $numberOfLines = 29;
 
-        if ($this->osInformationFetcher->isWindows())
-        {
-            /*
-             * Found no way to determine the number of lines in windows with pure php
-             * That's why a custom .exe file is used to determine the number of lines
-             */
-            $this->shellExecutor->executeCommand(__DIR__ . "\ConsoleHelper.exe printNumberOfRows");
-            $numberOfLines = (int)$this->shellExecutor->output()[0];
-        }
-        elseif ($this->osInformationFetcher->isLinux())
-        {
-            /*
-             * Using "stty" instead of "tput" here because "tput" will display a wrong value
-             * when redirecting stderr to stdout
-             */
-            $returnValue = $this->shellExecutor->executeCommand("stty size");
-            if ($returnValue === 0)
-            {
-                $dimensionsString = $this->shellExecutor->output()[0];
-                $dimensions = explode(" ", $dimensionsString);
-                $numberOfLines = (int)$dimensions[0];
-            }
-        }
+		    if ($this->osInformationFetcher->isWindows())
+		    {
+			    $this->shellExecutor->executeCommand("powershell -command \"&{\$H=get-host;\$H.ui.rawui.windowsize.height;}\"");
+			    $numberOfLines = (int)$this->shellExecutor->output()[0];
+		    }
+		    elseif ($this->osInformationFetcher->isLinux())
+		    {
+			    /*
+				 * Using "stty" instead of "tput" here because "tput" will display a wrong value
+				 * when redirecting stderr to stdout
+				 */
+			    $returnValue = $this->shellExecutor->executeCommand("stty size");
+			    if ($returnValue === 0)
+			    {
+				    $dimensionsString = $this->shellExecutor->output()[0];
+				    $dimensions = explode(" ", $dimensionsString);
+				    $numberOfLines = (int)$dimensions[0];
+			    }
+		    }
 
-        return $numberOfLines;
+		    $this::$cachedNumberOfShellLines = $numberOfLines;
+	    }
+
+        return $this::$cachedNumberOfShellLines;
     }
 
     /**
      * Returns the number of shell columns.
+     * The number of shell columns is only fetched once, after that the cached number of shell columns will be returned.
      *
      * @return int The number of shell columns
      */
     public function getNumberOfShellColumns(): int
     {
-        // 120 is the default number of columns in cmd and PowerShell
-        $numberOfColumns = 120;
+    	if (! $this::$cachedNumberOfShellColumns)
+	    {
+		    // 120 is the default number of columns in cmd and PowerShell
+		    $numberOfColumns = 120;
 
-        if ($this->osInformationFetcher->isWindows())
-        {
-            /*
-             * The custom .exe file is used here instead of "mode con /status" because that command
-             * will return a wrong number of columns in PowerShell when reducing the width of the window
-             */
-            $this->shellExecutor->executeCommand(__DIR__ . "\ConsoleHelper.exe printNumberOfColumns");
-            $numberOfColumns = (int)$this->shellExecutor->output()[0];
-        }
-        elseif ($this->osInformationFetcher->isLinux())
-        {
-            /*
-             * Using "stty" instead of "tput" here because "tput" will display a wrong value
-             * when redirecting stderr to stdout
-             */
-            $returnValue = $this->shellExecutor->executeCommand("stty size");
-            if ($returnValue === 0)
-            {
-                $dimensionsString = $this->shellExecutor->output()[0];
-                $dimensions = explode(" ", $dimensionsString);
-                $numberOfColumns = (int)$dimensions[1];
-            }
-        }
+		    if ($this->osInformationFetcher->isWindows())
+		    {
+			    $this->shellExecutor->executeCommand("powershell -command \"&{\$H=get-host;\$H.ui.rawui.windowsize.width;}\"");
+			    $numberOfColumns = (int)$this->shellExecutor->output()[0];
+		    }
+		    elseif ($this->osInformationFetcher->isLinux())
+		    {
+			    /*
+				 * Using "stty" instead of "tput" here because "tput" will display a wrong value
+				 * when redirecting stderr to stdout
+				 */
+			    $returnValue = $this->shellExecutor->executeCommand("stty size");
+			    if ($returnValue === 0)
+			    {
+				    $dimensionsString = $this->shellExecutor->output()[0];
+				    $dimensions = explode(" ", $dimensionsString);
+				    $numberOfColumns = (int)$dimensions[1];
+			    }
+		    }
 
-        return $numberOfColumns;
+		    $this::$cachedNumberOfShellColumns = $numberOfColumns;
+	    }
+
+        return $this::$cachedNumberOfShellColumns;
     }
 }
